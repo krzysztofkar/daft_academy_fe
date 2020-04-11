cafea8962fa7a0a36b158d41ceb5c1f28cca5490
Krzysiek Karczewski <krzysztofkarczewski92@gmail.com>
2020-04-11 10:26:31 +0200

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,20 @@ module.exports = {
         filename: 'index.js',
         path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
     plugins: [
         new HtmlWebPackPlugin({
             template: './src/index.html',
