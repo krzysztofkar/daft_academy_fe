53227c108be31129815d53f601c8f910dfda0098
Krzysiek Karczewski <krzysztofkarczewski92@gmail.com>
2020-04-27 23:47:44 +0200

    add function for carousel

--- functions.js
+++ functions.js
@@ -30,8 +30,42 @@ const showCurrentYear = () => {
     yearContainer.innerHTML += currentYear
 }
 
+const slider = () => {
+    const carousel = document.querySelector('.carousel')
+
+    const buttonPrev = document.querySelector('.carousel__button--prev')
+    const buttonNext = document.querySelector('.carousel__button--next')
+
+    const halfProductWidth = 170;
+
+    buttonPrev.addEventListener('click', () => {
+        let pixelsFromLeft = carousel.scrollLeft;
+
+        if ((pixelsFromLeft - halfProductWidth) <= 0) {
+            carousel.scrollTo(0, 0);
+        } else {
+            carousel.scrollTo((pixelsFromLeft - halfProductWidth), 0);
+        }
+    });
+
+    buttonNext.addEventListener('click', () => {
+        let pixelsFromLeft = carousel.scrollLeft
+        let carouselWidth = carousel.scrollWidth;
+
+        if ((pixelsFromLeft + halfProductWidth) >= carouselWidth) {
+            carousel.scrollTo(carouselWidth, 0);
+        } else {
+            carousel.scrollTo((pixelsFromLeft + halfProductWidth), 0);
+        }
+    });
+
+
+
+}
+
 export const functions = () => {
     scrollToTop()
     showAllProducts()
     showCurrentYear()
+    slider()
 }
