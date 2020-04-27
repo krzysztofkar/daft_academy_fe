0ad39f1353053dc132d84d4a3bca5dfa0b9dcb51
Krzysiek Karczewski <krzysztofkarczewski92@gmail.com>
2020-04-28 00:33:20 +0200

    fix All Products method

--- functions.js
+++ functions.js
@@ -13,14 +13,34 @@ const scrollToTop = () => {
     })
 };
 
+const createProductCards = quantity => {
+    let i = 0
+    const cards = []
+    while (i < quantity) {
+        const card = document.createElement('div');
+        card.innerHTML += `<img src="https://via.placeholder.com/309x390.png" alt="">
+        <div class="card-body" >
+            <div class="new-arrivals-container__product-name">product-name</div>
+            <div class="new-arrivals-container__product-price">product-price</div>
+            <div class="new-arrivals-container__cart-container">
+            <div class="new-arrivals-container__cart-container__add-to-cart">Add to cart</div>
+                <img src="../src/assets/heart.png" alt="">
+            </div>
+        </div>`;
+        card.setAttribute('class', 'card');
+        console.log(card)
+        cards.push(card)
+        i++;
+    }
+    return cards
+};
+
 const showAllProducts = () => {
     const allProductsButton = document.querySelector('.new-arrivals-container__all-products-button')
-    const hiddenProducts = document.querySelectorAll('.hidden-product');
+    const productsContainer = document.querySelector('.new-arrivals-container__products-container')
     allProductsButton.addEventListener('click', () => {
-        [...hiddenProducts].map(hiddenProduct => {
-            hiddenProduct.classList.remove('hidden-product')
-        })
-
+        const cards = createProductCards(4)
+        cards.map(card => productsContainer.appendChild(card))
     })
 }
 
